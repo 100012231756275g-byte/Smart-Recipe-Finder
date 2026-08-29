93f8c83252209dec96d36678a61fabfd8bdb938f
Ko <100012231756275g@gmail.com>
2026-08-29 23:41:17 +0700

    Export auth instance from firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNpo3I7cl0gi2UfCCNBDuCh8NO5kUh6hQ",
+  apiKey: "AIzaSyBnpO3i7C10gi2UFCCNBDucH8NO5KUhQhQ",
   authDomain: "smart-recipe-finder-b7c5e.firebaseapp.com",
   projectId: "smart-recipe-finder-b7c5e",
   storageBucket: "smart-recipe-finder-b7c5e.firebasestorage.app",
